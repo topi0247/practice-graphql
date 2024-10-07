fe5a2ad2af861047afb39618cb0755e05dfcdc53
topi0247 <karubona.ra247@gmail.com>
2024-10-07 17:04:46 +0900

    add:todos

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,8 +1,6 @@
 package graph
 
-// This file will not be regenerated automatically.
-//
-// It serves as dependency injection for your app, add any dependencies you require here.
+//go:generate go run github.com/99designs/gqlgen generate
 import "github.com/topi0247/practice-graphql/graph/model"
 
 type Resolver struct{
